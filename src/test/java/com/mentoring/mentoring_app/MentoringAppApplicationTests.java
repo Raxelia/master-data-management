548102da8a0538e25aa64ace144341cd5d63490d
523340_genpt <Joanna_Bankowska@genpt.com>
2025-03-12 20:30:10 +0100

    init

---
+++ src/test/java/com/mentoring/mentoring_app/MentoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoring.mentoring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
